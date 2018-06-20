5c3195e51941d5dfc94b96034052dd3ad2db3285
Romina D-R <rdr2711@yahoo.com>
2018-06-20 20:34:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const welcomeMessage =
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, $`
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
