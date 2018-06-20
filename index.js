c389aaa3bded987716da9f831f450fb852e0c684
Romina D-R <rdr2711@yahoo.com>
2018-06-20 20:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const currentUser = 'Dumb romi';
 const welcomeMessage = 
 `Welcome to Klossybook, ${currentUser}!`;
 
-const excitedWelcomeMessage = `WELCOME TO KLOSSYBOOK, ${currentUser}, `
+const excitedWelcomeMessage = `WELCOME TO KLOSSYBOOK, ${currentUser}!`;
+
